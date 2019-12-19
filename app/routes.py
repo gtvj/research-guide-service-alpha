b980fc49243140eabf96890aa40d2c11709763fa
Gwyn Jones <m@gwnjns.com>
2019-12-19 11:08:02 +0000

    Add some data and (very) basic routes
    The data added is a huge JSON file containing all the references that appear
    in research guides, along with backlinks. For testing purposes a smaller 'sample'
    file has also been added.
    
    This provides a home route (that returns a home page) and a very simple
    implementation of a `/guides/<ref>` route that returns a JSON object, provided
    a match is found at the root of the JSON.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
 from flask import render_template
+import json
 
 @app.route('/')
 def home():
-    return '...'
+    return render_template('home.html', title='Research guide service: Flask implementation')
+
+@app.route('/guides/<ref>')
+def guides(ref):
+    with open('./app/data/references_in_guides_backlinked_deduped.json') as guides:
+        guides = json.load(guides)
+
+        if ref in guides:
+            return guides[ref]
+            
+        return 'Nowt'
+        
+    
