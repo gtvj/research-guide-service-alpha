339c86450cb367f549997236efa1863ea7857c67
Gwyn Jones <m@gwnjns.com>
2019-12-30 13:09:50 +0000

    Add get_guides_for_series and test

--- get_guides.py
+++ get_guides.py
@@ -1,10 +1,14 @@
 import json
 
-
 with open('./app/data/references_in_guides_backlinked_deduped.json') as guides:
     guides = json.load(guides)
 
-def get_guides_for_lettercode(letter_code):
 
-    if(letter_code and guides[letter_code]):
+def get_guides_for_lettercode(letter_code):
+    if (letter_code and guides[letter_code]):
         return guides[letter_code]
+
+
+def get_guides_for_series(series, guides_for_lettercode):
+    if (series and guides_for_lettercode["records"][series]):
+        return guides_for_lettercode["records"][series]
--- test/get_guides_test.py
+++ test/get_guides_test.py
@@ -1,7 +1,7 @@
 from get_guides import *
 
-def test_get_guides_for_letter_code():
 
+def test_get_guides_for_letter_code():
     result = get_guides_for_lettercode('AB')
 
     expected = {
@@ -34,6 +34,51 @@ def test_get_guides_for_letter_code():
                     "photographs": "Photographs"
                 }
             }
+        }
+    }
+
+    assert result == expected
+
+
+def test_get_guides_for_series():
+    guides_for_lettercode = {
+        "guides": {
+            "contaminated-land": "Contaminated land"
+        },
+        "records": {
+            "AB 11": {
+                "guides": {
+                    "photographs": "Photographs"
+                }
+            },
+            "AB 13": {
+                "guides": {
+                    "photographs": "Photographs"
+                }
+            },
+            "AB 18": {
+                "guides": {
+                    "photographs": "Photographs"
+                }
+            },
+            "AB 5": {
+                "guides": {
+                    "architectural-drawings": "Architectural drawings"
+                }
+            },
+            "AB 7": {
+                "guides": {
+                    "photographs": "Photographs"
+                }
+            }
+        }
+    }
+
+    result = get_guides_for_series('AB 13', guides_for_lettercode)
+
+    expected = {
+        "guides": {
+            "photographs": "Photographs"
         }
     }
 
