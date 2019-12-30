1ce635f2eb86fae30530c55c9307eb1282a399e4
Gwyn Jones <m@gwnjns.com>
2019-12-30 14:29:12 +0000

    Add get_guides_for_series

--- get_guides.py
+++ get_guides.py
@@ -4,11 +4,23 @@ with open('./app/data/references_in_guides_backlinked_deduped.json') as guides:
     guides = json.load(guides)
 
 
-def get_guides_for_lettercode(letter_code):
-    if (letter_code and guides[letter_code]):
-        return guides[letter_code]
+def get_guides_for_lettercode(lettercode, guides):
+    if (lettercode and guides[lettercode]):
+        return guides[lettercode]
+    return ''
 
 
-def get_guides_for_series(series, guides_for_lettercode):
-    if (series and guides_for_lettercode["records"][series]):
-        return guides_for_lettercode["records"][series]
+def get_guides_for_series(lettercode, series, guides):
+    lettercode_found = guides[lettercode] or False
+
+    if (lettercode_found):
+        series_found = guides[lettercode]["records"][series] or False
+
+        if (series_found):
+            return guides[lettercode]["records"][series]
+
+    return ''
+#
+# def get_guides_for_reference(reference, guides):
+#     if (reference and guides["records"][reference]):
+#         return guides["records"][reference]
--- test/get_guides_test.py
+++ test/get_guides_test.py
@@ -1,8 +1,11 @@
 from get_guides import *
 
+with open('./app/data/references_in_guides_backlinked_deduped.json') as guides:
+    guides = json.load(guides)
+
 
 def test_get_guides_for_letter_code():
-    result = get_guides_for_lettercode('AB')
+    result = get_guides_for_lettercode('AB', guides)
 
     expected = {
         "guides": {
@@ -41,40 +44,7 @@ def test_get_guides_for_letter_code():
 
 
 def test_get_guides_for_series():
-    guides_for_lettercode = {
-        "guides": {
-            "contaminated-land": "Contaminated land"
-        },
-        "records": {
-            "AB 11": {
-                "guides": {
-                    "photographs": "Photographs"
-                }
-            },
-            "AB 13": {
-                "guides": {
-                    "photographs": "Photographs"
-                }
-            },
-            "AB 18": {
-                "guides": {
-                    "photographs": "Photographs"
-                }
-            },
-            "AB 5": {
-                "guides": {
-                    "architectural-drawings": "Architectural drawings"
-                }
-            },
-            "AB 7": {
-                "guides": {
-                    "photographs": "Photographs"
-                }
-            }
-        }
-    }
-
-    result = get_guides_for_series('AB 13', guides_for_lettercode)
+    result = get_guides_for_series('AB', 'AB 11', guides)
 
     expected = {
         "guides": {
