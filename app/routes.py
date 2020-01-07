c1a19988eb1f35499a7bdaa42229e8a18b68b942
Gwyn Jones <m@gwnjns.com>
2020-01-07 10:56:27 +0000

    Update routes ready for deployment to cloud

--- app/routes.py
+++ app/routes.py
@@ -4,29 +4,36 @@ from get_fragments import *
 from get_guides import *
 from get_content_for_guides import *
 
+@app.route('/ping')
+def ping():
+    return 'OK', 200
+
+@app.route('/')
+def root():
+    return 'You should not be here. The base route is /research-guide-service'
 
 # Service routes
-@app.route('/')
+@app.route('/research-guide-service/')
 def home():
     return render_template('home.html', title='Research guide service')
 
 
-@app.route('/fragments/<path:ref>')
+@app.route('/research-guide-service/fragments/<path:ref>')
 def fragments(ref):
     return get_fragments(ref)
 
 
-@app.route('/guides/<path:ref>')
+@app.route('/research-guide-service/guides/<path:ref>')
 def related_guides(ref):
     return get_guides(ref)
 
 
-@app.route('/guides_content/')
+@app.route('/research-guide-service/guides_content/')
 def no_content():
     return 'No reference provided'
 
 
-@app.route('/guides_content/<path:ref>')
+@app.route('/research-guide-service/guides-content/<path:ref>')
 def guide_content_for_reference(ref):
     guides = get_guides(ref)
 
@@ -38,18 +45,18 @@ def guide_content_for_reference(ref):
 
 
 # No reference provided to endpoint
-@app.route('/guides/')
-@app.route('/fragments/')
-@app.route('/guides_content/')
+@app.route('/research-guide-service/guides/')
+@app.route('/research-guide-service/fragments/')
+@app.route('/research-guide-service/guides-content/')
 def no_guides():
     return render_template('error.html', title='No reference provided', endpoint=request.path)
 
 
 # Service playground
-@app.route('/service_playground/', methods=['POST', 'GET'])
+@app.route('/research-guide-service/service-playground/', methods=['POST', 'GET'])
 def service_playground():
     if request.method == 'POST':
-        destination = "/%s/%s" % (request.form['endpoint_type'], request.form['catalogue_reference'])
+        destination = "/research-guide-service/%s/%s" % (request.form['endpoint_type'], request.form['catalogue_reference'])
         return redirect(destination)
     else:
         return render_template('endpoint_playground.html')
